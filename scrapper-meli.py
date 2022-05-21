83bbaf6036c8b777ac3df3b709e34fb2afda8d4e
Maria Agustina Markosich <agustina@noblypos.com>
2022-05-21 18:42:53 -0300

    Scrapper meli, carteras cuero, carteras gamuza

--- scrapper-meli.py
+++ scrapper-meli.py
@@ -1,25 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import cv2
 
-url = 'https://listado.mercadolibre.com.ar/cartera-cuero#D%5BA:cartera%20cuero%5D'
-folder_name = 'cartera-cuero'
-
-print("URL A DESCARGAR: ", url)
-
-try:
-	os.mkdir(os.path.join(os.getcwd(), folder_name))
-except:
-	"Error creating directory"
-	pass
+url = 'https://bolsos.mercadolibre.com.ar/carteras-billeteras/cartera-gamuza#D%5BA:cartera%20gamuza,on%5D'
+folder_name = 'cartera-gamuza'
 
 os.chdir(os.path.join(os.getcwd(), folder_name))
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-images = soup.find_all('img')
-
-print(images)
+images = soup.find_all('img',{"class": "ui-search-result-image__element"})
 
 for image in images:
 	try:
@@ -32,4 +23,13 @@ for image in images:
 	except BaseException as error:
 		print("Error scrapping image",error)
 		
-
+print(os.listdir(os.getcwd()))
+for file in os.listdir(os.getcwd()):
+	print(file)
+	try:
+		img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+		h, w = img.shape
+		if(not (h > 100 or w > 100)):
+			os.remove(file)
+	except:
+		print("Error getting image size")
